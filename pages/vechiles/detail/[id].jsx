661e6d05a12f2657759fca16f5ddc0b5c9d16372
Chandra-Kurnia <chandrakqurniawan2002@gmail.com>
2021-08-23 07:22:42 +0700

    feature admin add to homepage & remove from homepage

--- pages/vechiles/detail/[id].jsx
+++ pages/vechiles/detail/[id].jsx
@@ -12,30 +12,31 @@ import {useEffect} from 'react';
 import axios from 'axios';
 import swal from 'sweetalert';
 
-export const getServerSideProps = async(context) => {
-  try{
-    const vehicleId = context.query.id
-    const res = await axios.get(`${process.env.API_SERVER}/vehicle/${vehicleId}`)
-    const vehicle = res.data.data
-    return{
-      props: {vehicle}
-    }
-  }catch(error){
-    swal('Error', 'Error during get data from server', 'error')
+export const getServerSideProps = async (context) => {
+  try {
+    const vehicleId = context.query.id;
+    const res = await axios.get(`${process.env.API_SERVER}/vehicle/${vehicleId}`);
+    const popularvehicle = await axios.get(`${process.env.API_SERVER}/vehicle/4/popular`);
+    const vehicle = res.data.data;
+    const populars = popularvehicle.data.data;
+    return {
+      props: {vehicle, populars},
+    };
+  } catch (error) {
+    swal('Error', 'Error during get data from server', 'error');
   }
-}
+};
 
 const Show = (props) => {
-  const vehicle = props.vehicle
+  const vehicle = props.vehicle;
+  const populars = props.populars;
   const admin = true;
   const {query, back, push} = useRouter();
-  // const [vehicle, setvehicle] = useState('');
   let [amount, setamount] = useState(0);
   useEffect(() => {
-    axios
-      .get(`${process.env.API_SERVER}/vehicle/${query.id}`)
-      // .then((result) => setvehicle(result.data.data))
-      // .catch((err) => alert(err.response.data.error[0].msg));
+    axios.get(`${process.env.API_SERVER}/vehicle/${query.id}`);
+    // .then((result) => setvehicle(result.data.data))
+    // .catch((err) => alert(err.response.data.error[0].msg));
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   const handlePlus = () => {
@@ -52,7 +53,33 @@ const Show = (props) => {
       setamount(amount - 1);
     }
   };
-  
+
+  const addtohomepage = () => {
+    axios
+      .get(`${process.env.API_SERVER}/vehicle/addtohomepage/${query.id}`)
+      .then(() => {
+        swal('Success', 'Successfully add vehicle to homepage', 'success').then(() => {
+          push('/');
+        });
+      })
+      .catch(() => {
+        swal('Error!', 'Failed to add vehicle to home page, please try again later', 'error');
+      });
+  };
+
+  const removefromhomepage = () => {
+    axios
+      .get(`${process.env.API_SERVER}/vehicle/removefromhomepage/${query.id}`)
+      .then(() => {
+        swal('Success', 'Successfully remvoe vehicle from homepage', 'success').then(() => {
+          push('/');
+        });
+      })
+      .catch(() => {
+        swal('Error!', 'Failed to add vehicle to home page, please try again later', 'error');
+      });
+  }
+
   return (
     <Fragment>
       <Layout title="Zembrani | vechiles">
@@ -107,11 +134,14 @@ const Show = (props) => {
             <div className="d-flex flex-wrap justify-content-lg-start">
               {admin === true ? (
                 <>
-                  <ButtonAuth
-                    onClick={() => push({pathname: `/admin/update/${vehicle?.vehicle_id}`})}
-                    bgcolor="bg-black"
-                    text="Add to home page"
-                  />
+                  {populars[0].vehicle_id === vehicle.vehicle_id ||
+                  populars[1].vehicle_id === vehicle.vehicle_id ||
+                  populars[2].vehicle_id === vehicle.vehicle_id ||
+                  populars[3].vehicle_id === vehicle.vehicle_id ? (
+                    <ButtonAuth onClick={() => removefromhomepage()} bgcolor="bg-black" text="Remove from home page" />
+                  ) : (
+                    <ButtonAuth onClick={() => addtohomepage()} bgcolor="bg-black" text="Add to home page" />
+                  )}
                   <ButtonAuth
                     onClick={() => push({pathname: `/admin/update/${vehicle?.vehicle_id}`})}
                     bgcolor="bg-orange"
